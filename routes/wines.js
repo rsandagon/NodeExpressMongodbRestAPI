3321992c51b72bf65237051a86918f8b8a5f5c08
Ryan Sandagon <ryan.sandagon@englishcentral.com>
2013-05-24 17:49:05 +0800

    fixed, comma problem.

--- routes/wines.js
+++ routes/wines.js
@@ -2,7 +2,7 @@ var mongo = require('mongodb');
 
 var Server = mongo.Server,
 	Db = mongo.Db,
-	BSON = mongo.BSONPure,
+	BSON = mongo.BSONPure;
 
 var server = new Server('localhost',27017,{auto_reconnect: true});
 db = new Db('winedb',server);
